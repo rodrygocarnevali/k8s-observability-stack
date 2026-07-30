74ab98b8ea2b4a0ac8c423425a637f96777e29ed
Rodrygo Carnevali <rodrygovilela@gmail.com>
2026-07-30 13:52:09 -0300

    mudando pra disponibilizar metricas

--- demo-api/main.go
+++ demo-api/main.go
@@ -3,8 +3,31 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
+var requestsTotal = prometheus.NewCounter(
+	prometheus.CounterOpts{
+		Name: "demo_api_requests_total",
+		Help: "Total de requisicoes recebidas pela aplicacao.",
+	},
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	requestsTotal.Inc()
+
+	fmt.Fprintln(w, "Hello DevOps!")
+}
+
 func main() {
-	fmt.Println("Iniciando servidor...")
+	prometheus.MustRegister(requestsTotal)
+
+	http.HandleFunc("/", handler)
+	http.Handle("/metrics", promhttp.Handler())
+
+	fmt.Println("Servidor iniciado na porta 8080")
+
+	http.ListenAndServe(":8080", nil)
 }
